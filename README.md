0d44735b5698804edd197126af29610bf5a834fc
killian31 <80256683+killian31@users.noreply.github.com>
2023-12-01 15:24:44 +0100

    feat: shiny app and update readme

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Case Study of Climate Change on Migrations
 
 ## Getting the data
-First, install the package `nasapower`, and run the R script `get_data.R`.
 
-## Abstract
+First, install the required packages by running
+``` r
+install.packages(c("shiny", "leaflet", "nasapower", "glue", "dplyr"))
+```
+Then in RStudio, open the file `get.data.R` and lauch the shinyApp by clicking on the "RunApp" button.
+Click on the map for your location of interest, and then click on the download button to download the data.
--- get_data.R
+++ get_data.R
@@ -1,11 +1,79 @@
-library("nasapower")
-# help("get_power") # montre la vignette de la fonction
+# Install and load required packages
+# install.packages(c("shiny", "leaflet", "nasapower", "glue", "dplyr"))
+library(shiny)
+library(leaflet)
+library(dplyr)
+library(nasapower)
+library(glue)
 
-# Récupérer les variables d'intérêt Tmin, Tmax, Tmean
-daily_single_ag <- get_power(
-  community = "ag",
-pars = c("T2M_MIN", "T2M_MAX", "T2M"),
-  lonlat = c(151.81, -27.48),
-  temporal_api = "daily",
-  dates = c("1985-01-01", "2023-01-31")
+ui <- fluidPage(
+  titlePanel("NASA Power Explorer"),
+  fluidRow(
+    column(
+      width = 12,
+      h4("Click on the map and download the temperature data for the clicked coordinates."),
+    )
+  ),
+  fluidRow(
+    column(width = 12, leafletOutput("map"))
+  ),
+  br(),
+  fluidRow(
+    column(width = 12, actionButton("use_clik_loc", "Download Data from Clicked Location", class = "btn-primary"))
+  ),
+  br(),
+  fluidRow(
+    column(width = 12, actionButton("exit_app", "Exit App", class = "btn-danger"))
+  ),
+  tags$style(HTML(".leaflet {height: 80vh;}")),  # Adjust the height of the leaflet map
+  tags$style(HTML(".btn-primary, .btn-danger {width: 100%;}"))  # Make buttons occupy the full width
 )
+
+server <- function(input, output, session) {
+  output$map <- renderLeaflet(addTiles(leaflet()))
+  
+  observeEvent(input$use_clik_loc, {
+    last_click <- isolate(as.data.frame(input$map_click))
+    lonlat = c(last_click$lng, last_click$lat)
+    
+    # Display a message about the selected location
+    showNotification(
+      glue("Downloading data for location: {lonlat[1]}, {lonlat[2]}"),
+      duration = 5,
+      type = "message"
+    )
+    
+    # Récupérer les variables d'intérêt Tmin, Tmax, Tmean
+    daily_single_ag <- get_power(
+      community = "ag",
+      pars = c("T2M_MIN", "T2M_MAX", "T2M"),
+      lonlat = lonlat,
+      temporal_api = "daily",
+      dates = c("1985-01-01", "2023-01-31")
+    )
+    
+    # Save the dataset to a CSV file
+    write.csv(daily_single_ag, glue("./dataset_{lonlat[1]}_{lonlat[2]}.csv"), row.names = FALSE)
+    
+    # Display a success message
+    showNotification(
+      "Data downloaded successfully!",
+      duration = 5,
+      type = "default"
+    )
+  })
+  
+  observeEvent(input$exit_app, {
+    # Display a message before exiting
+    showNotification(
+      "Exiting the application...",
+      duration = 5,
+      type = "message"
+    )
+    # Delay for a moment before exiting
+    Sys.sleep(1)
+    stopApp(session)
+  })
+}
+
+shinyApp(ui, server)

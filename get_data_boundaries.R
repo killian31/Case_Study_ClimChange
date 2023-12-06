deafecb446ff018d8f0c8042c3233496a74358eb
killian31 <80256683+killian31@users.noreply.github.com>
2023-12-06 10:33:27 +0100

    chore: get_data_boundaries as functions storing file

--- get_data_boundaries.R
+++ get_data_boundaries.R
@@ -4,16 +4,16 @@ library(tidyr)
 library(dplyr)
 library(ggplot2)
 
-GeoDATA <- read_sf("./world-administrative-boundaries.geojson")
-GeoDATA <- st_transform(GeoDATA, crs = '+proj=robin +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs')
-GeoDATA.WGS84 <- st_transform(GeoDATA, crs = '+proj=longlat +datum=WGS84')
-Ukraine <- GeoDATA.WGS84[GeoDATA.WGS84$iso3 == "UKR", ]
+# GeoDATA <- read_sf("./world-administrative-boundaries.geojson")
+# GeoDATA <- st_transform(GeoDATA, crs = '+proj=robin +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs')
+# GeoDATA.WGS84 <- st_transform(GeoDATA, crs = '+proj=longlat +datum=WGS84')
+# Ukraine <- GeoDATA.WGS84[GeoDATA.WGS84$iso3 == "UKR", ]
 # Get bounding box
-bbox_list <- as.list(st_bbox(Ukraine))
-xmin <- bbox_list$xmin
-ymin <- bbox_list$ymin
-xmax <- bbox_list$xmax
-ymax <- bbox_list$ymax
+# bbox_list <- as.list(st_bbox(Ukraine))
+# xmin <- bbox_list$xmin
+# ymin <- bbox_list$ymin
+# xmax <- bbox_list$xmax
+# ymax <- bbox_list$ymax
 
 # compute_smaller_bbox <- function(xmin, ymin, xmax, ymax, target_area = 5) {
 #   # Calculate the original bounding box dimensions
@@ -65,68 +65,36 @@ centered_bounding_box <- function(xmin, ymin, xmax, ymax) {
   return(c(new_xmin, new_ymin, new_xmax, new_ymax))
 }
 
-original_bbox = c(xmin, ymin, xmax, ymax)
-smaller_bbox <- centered_bounding_box(xmin, ymin, xmax, ymax)
-
-daily_single_2022 <- get_power(
-  community = "ag",
-  pars = c("T2M_MIN", "T2M_MAX", "T2M"),
-  lonlat = smaller_bbox,
-  temporal_api = "daily",
-  dates = c("2022-01-01", "2022-12-31")
-)
-colnames(daily_single_2022)
-
-calculate_statistics <- function(data, variable) {
-  stats <- summary(data[[variable]])
-  quantiles <- quantile(data[[variable]], c(0.05, 0.10, 0.5, 0.90, 0.95))
-  return(c(Minimum = stats[1], Mean = stats[3], Maximum = stats[6], Quantiles = quantiles))
-}
-
-# Function to get climate data for a specific variable and perform calculations
-get_and_analyze_climate_data <- function(community, variable, smaller_bbox, year) {
-  # Get climate data
-  daily_data <- get_power(
-    community = community,
-    pars = variable,
+get_yearly_data <- function(year, iso3 = "UKR", geodata_file = "./world-administrative-boundaries.geojson") {
+  GeoDATA <- read_sf(geodata_file)
+  GeoDATA <- st_transform(GeoDATA, crs = '+proj=robin +lon_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs')
+  GeoDATA.WGS84 <- st_transform(GeoDATA, crs = '+proj=longlat +datum=WGS84')
+  country <- GeoDATA.WGS84[GeoDATA.WGS84$iso3 == iso3, ]
+  
+  bbox_list <- as.list(st_bbox(country))
+  xmin <- bbox_list$xmin
+  ymin <- bbox_list$ymin
+  xmax <- bbox_list$xmax
+  ymax <- bbox_list$ymax
+  
+  smaller_bbox <- centered_bounding_box(xmin, ymin, xmax, ymax)
+  
+  daily_single <- get_power(
+    community = "ag",
+    pars = c("T2M_MIN", "T2M_MAX", "T2M"),
     lonlat = smaller_bbox,
     temporal_api = "daily",
-    dates = c(paste(year, "-01-01"), paste(year, "-12-31"))
+    dates = c(paste(year,"-01-01", sep=""), paste(year,"12-31", sep=""))
   )
-  
-  # Calculate summary statistics
-  statistics <- calculate_statistics(daily_data, variable)
-  
-  # Return results
-  return(data.frame(Year = year, Variable = variable, statistics))
+  mean_daily <- daily_single %>%
+    group_by(YEAR, MM, DD) %>%
+    summarise(
+      LAT = mean(LAT),
+      LON = mean(LON),
+      T2M_MIN = mean(T2M_MIN),
+      T2M_MAX = mean(T2M_MAX),
+      T2M = mean(T2M)
+    ) %>%
+    ungroup()
+  return(mean_daily)
 }
-
-community <- "ag"
-variables <- c("T2M_MIN", "T2M_MAX", "T2M")
-year <- 2022
-
-final_results <- data.frame(matrix(ncol = 10, nrow = 0))
-colnames(final_results) <- c("Year", "Variable", "Minimum", "Mean", "Maximum", "Quantile_0.05", "Quantile_0.10", "Quantile_0.5", "Quantile_0.90", "Quantile_0.95")
-
-# Loop through each variable
-for (variable in variables) {
-  # Get and analyze climate data for the variable
-  variable_results <- get_and_analyze_climate_data(community, variable, smaller_bbox, year)
-  
-  # Append results to the final data frame
-  final_results <- rbind(final_results, variable_results)
-}
-
-# Convert results to a tibble for easier manipulation
-final_results_tibble <- as_tibble(final_results)
-
-# Print or further analyze the final results
-print(final_results_tibble)
-
-# Plotting the results for each variable
-ggplot(final_results_tibble, aes(x = Variable, y = statistics, fill = Variable)) +
-  geom_bar(stat = "identity") +
-  labs(title = "Mean Temperature in 2022",
-       x = "Variable",
-       y = "Mean Temperature") +
-  theme_minimal()

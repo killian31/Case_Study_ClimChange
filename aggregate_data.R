0ede7017106bf65749db32d1dc8d916f4750f505
killian31 <80256683+killian31@users.noreply.github.com>
2024-01-10 23:15:58 +0100

    fix: boxplots script

--- aggregate_data.R
+++ aggregate_data.R
@@ -5,6 +5,38 @@ data = read.csv("data/temperature_daily_grid_UKR.csv", header = TRUE, sep = ",")
 head(data)
 tail(data)
 
+drawYearlyBoxplots <- function(data, temp_variable, file_name) {
+  # Check if the temperature variable is valid
+  if (!(temp_variable %in% c("temperature_mean", "temperature_min", "temperature_max"))) {
+    stop("Invalid temperature variable. Choose from 'temperature_mean', 'temperature_min', 'temperature_max'.")
+  }
+  
+  # Extract year from the date column
+  data$year <- as.integer(format(as.Date(data$date), "%Y"))
+  
+  # Prepare the data: create a subset with only the relevant temperature variable and the year
+  data_for_plot <- data[c("year", temp_variable)]
+  
+  # Renaming the temperature column to a generic name for plotting
+  names(data_for_plot)[names(data_for_plot) == temp_variable] <- "Temperature"
+  
+  # Open a JPEG device
+  jpeg(file_name, width = 1600, height = 900) # You can adjust the size as needed
+  
+  # Using boxplot to plot the data
+  boxplot(Temperature ~ year, data = data_for_plot, 
+          xlab = "Year", ylab = "Temperature", 
+          main = paste("Yearly Boxplots of", temp_variable),
+          las = 2, # Rotates x-axis labels
+          col = rainbow(length(unique(data_for_plot$year))) # Optional: Adds color
+  )
+  
+  # Close the device
+  dev.off()
+}
+
+
+
 calculateYearlyStatistics <- function(data, start_year, end_year, temp_variable) {
   # Check if the temperature variable is valid
   if (!(temp_variable %in% c("temperature_mean", "temperature_min", "temperature_max"))) {
@@ -47,7 +79,14 @@ calculateYearlyStatistics <- function(data, start_year, end_year, temp_variable)
   return(results)
 }
 
+drawYearlyBoxplots(data, "temperature_mean", "boxplot_mean.jpg")
+drawYearlyBoxplots(data, "temperature_min", "boxplot_min.jpg")
+drawYearlyBoxplots(data, "temperature_max", "boxplot_max.jpg")
+
 final_dataset_mean <- calculateYearlyStatistics(data, 1990, 2020, "temperature_mean")
 write.csv(final_dataset_mean, "aggregate_mean.csv", row.names=FALSE)
 final_dataset_min <- calculateYearlyStatistics(data, 1990, 2020, "temperature_min")
+write.csv(final_dataset_min, "aggregate_min.csv", row.names=FALSE)
 final_dataset_max <- calculateYearlyStatistics(data, 1990, 2020, "temperature_max")
+write.csv(final_dataset_max, "aggregate_max.csv", row.names=FALSE)
+

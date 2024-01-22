c8b0871752cc7cca94b3ca0dbfafb480a7c96957
theodruilhe <83651483+theodruilhe@users.noreply.github.com>
2024-01-22 19:04:08 +0100

    Choose markers for the four countries

--- geodetic_markers.R
+++ geodetic_markers.R
@@ -1,5 +1,3 @@
-setwd(/Users/theodruilhe/Documents/M1-ECO-STATS/case_study)
-
 # Load data
 data_UKR <- read.csv("data/temperature_daily_grid_UKR.csv", header = TRUE, sep = ",")
 data_ESP <- read.csv("data/temperature_daily_grid_ESP.csv", header = TRUE, sep = ",")
@@ -10,6 +8,7 @@ data_PRT <- read.csv("data/temperature_daily_grid_PRT.csv", header = TRUE, sep =
 library(leaflet)
 library(dplyr)
 
+#### Ukrain ####
 data_UKR_distinct <- data_UKR %>%
   distinct(lat, lon)
 # Créer la carte avec leaflet
@@ -47,7 +46,24 @@ data_UKR_filtered <- data_UKR %>%
       (lat == 49.5 & lon == 38.75)
   )
 
-ma_carte <- leaflet(data_UKR_filtered) %>%
+data_UKR_filtered_distinct <- data_UKR_filtered %>%
+  distinct(lat, lon)
+
+carte_UKR <- leaflet(data_UKR_filtered_distinct) %>%
+  addTiles()  # Ajouter des tuiles pour le fond de la carte (OpenStreetMap)
+
+# Ajouter des marqueurs pour chaque point
+carte_UKR <- carte_UKR %>%
+  addMarkers(lng = ~lon, lat = ~lat, popup = ~paste("Latitude: ", lat, "<br>Longitude: ", lon))
+
+# Afficher la carte
+carte_UKR
+
+#### Espagne ####
+data_ESP_distinct <- data_ESP %>%
+  distinct(lat, lon)
+
+ma_carte <- leaflet(data_ESP_distinct) %>%
   addTiles()  # Ajouter des tuiles pour le fond de la carte (OpenStreetMap)
 
 # Ajouter des marqueurs pour chaque point
@@ -57,3 +73,130 @@ ma_carte <- ma_carte %>%
 # Afficher la carte
 ma_carte
 
+# point nord montagneux Latitude: 42.5, Longitude: 1.25
+# point sud Latitude: 37.5, Longitude: -5.625
+# point central vers madrid Latitude: 40, Longitude: -3.75
+# point nord ouest Latitude: 43, Longitude: -8.125
+
+data_ESP_north <- data_ESP %>%
+  filter(lat == 42.5, lon == 1.25)
+
+data_ESP_south <- data_ESP %>%
+  filter(lat == 37.5, lon == -5.625)
+
+data_ESP_center <- data_ESP %>%
+  filter(lat == 40, lon == -3.75)
+
+data_ESP_west <- data_ESP %>%
+  filter(lat == 43, lon == -8.125)
+
+data_ESP_filtered <- data_ESP %>%
+  filter(
+    (lat == 42.5 & lon == 1.25) |
+      (lat == 37.5 & lon == -5.625) |
+      (lat == 40 & lon == -3.75) |
+      (lat == 43 & lon == -8.125)
+  )
+
+data_ESP_filtered_distinct <- data_ESP_filtered %>%
+  distinct(lat, lon)
+
+carte_ESP <- leaflet(data_ESP_filtered_distinct) %>%
+  addTiles()  # Ajouter des tuiles pour le fond de la carte (OpenStreetMap)
+
+# Ajouter des marqueurs pour chaque point
+carte_ESP <- carte_ESP %>%
+  addMarkers(lng = ~lon, lat = ~lat, popup = ~paste("Latitude: ", lat, "<br>Longitude: ", lon))
+
+# Afficher la carte
+carte_ESP
+
+#### Pologne ####
+data_POL_distinct <- data_POL %>%
+  distinct(lat, lon)
+
+ma_carte <- leaflet(data_POL_distinct) %>%
+  addTiles()  # Ajouter des tuiles pour le fond de la carte (OpenStreetMap)
+
+# Ajouter des marqueurs pour chaque point
+ma_carte <- ma_carte %>%
+  addMarkers(lng = ~lon, lat = ~lat, popup = ~paste("Latitude: ", lat, "<br>Longitude: ", lon))
+
+# Afficher la carte
+ma_carte
+
+# point nord Latitude: 54.5 Longitude: 18.125
+# point continental Latitude: 50 Longitude: 21.25
+
+data_POL_north <- data_POL %>%
+  filter(lat == 54.5, lon == 18.125)
+
+data_POL_center <- data_POL %>%
+  filter(lat == 50, lon == 21.25)
+
+data_POL_filtered <- data_POL %>%
+  filter(
+    (lat == 54.5 & lon == 18.125) |
+      (lat == 50 & lon == 21.25)
+  )
+
+data_POL_filtered_distinct <- data_POL_filtered %>%
+  distinct(lat, lon)
+
+carte_POL <- leaflet(data_POL_filtered_distinct) %>%
+  addTiles()  # Ajouter des tuiles pour le fond de la carte (OpenStreetMap)
+
+# Ajouter des marqueurs pour chaque point
+carte_POL <- carte_POL %>%
+  addMarkers(lng = ~lon, lat = ~lat, popup = ~paste("Latitude: ", lat, "<br>Longitude: ", lon))
+
+# Afficher la carte
+carte_POL
+
+#### Portugal ####
+data_PRT_distinct <- data_PRT %>%
+  distinct(lat, lon)
+
+ma_carte <- leaflet(data_PRT_distinct) %>%
+  addTiles()  # Ajouter des tuiles pour le fond de la carte (OpenStreetMap)
+
+# Ajouter des marqueurs pour chaque point
+ma_carte <- ma_carte %>%
+  addMarkers(lng = ~lon, lat = ~lat, popup = ~paste("Latitude: ", lat, "<br>Longitude: ", lon))
+
+# Afficher la carte
+ma_carte
+
+# point nord Latitude: 41.5, Longitude: -6.875
+# point ouest Latitude: 39, Longitude: -9.375
+# point sud Latitude: 37.5, Longitude: -8.125
+
+data_PRT_north <- data_PRT %>%
+  filter(lat == 41.5, lon == -6.875)
+
+data_PRT_west <- data_PRT %>%
+  filter(lat == 39, lon == -9.375)
+
+data_PRT_south <- data_PRT %>%
+  filter(lat == 37.5, lon == -8.125)
+
+data_PRT_filtered <- data_PRT %>%
+  filter(
+    (lat == 41.5 & lon == -6.875) |
+      (lat == 39 & lon == -9.375) |
+      (lat == 37.5 & lon == -8.125)
+  )
+
+data_PRT_filtered_distinct <- data_PRT_filtered %>%
+  distinct(lat, lon)
+
+carte_PRT <- leaflet(data_PRT_filtered_distinct) %>%
+  addTiles()  # Ajouter des tuiles pour le fond de la carte (OpenStreetMap)
+
+# Ajouter des marqueurs pour chaque point
+carte_PRT <- carte_PRT %>%
+  addMarkers(lng = ~lon, lat = ~lat, popup = ~paste("Latitude: ", lat, "<br>Longitude: ", lon))
+
+# Afficher la carte
+carte_PRT
+  
